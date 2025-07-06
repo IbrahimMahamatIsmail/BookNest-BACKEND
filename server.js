349586bae5f9bf6def9a91fdc5fa51f48fb729c0
IbrahimMahamatIsmail <65311905ibrahim@gmail.com>
2025-07-07 00:24:16 +0200

    Mise à jour du uploads

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use('/livres', require('./routers/livresRoute'));
 app.use('/utilisateurs', require('./routers/utilisateursRoute'));
 
 // Rendre le dossier public accessible publiquement
-app.use('/uploads', express.static(path.join(__dirname, 'public', 'uploads')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
 // Test
 app.get('/', (req, res) => {
